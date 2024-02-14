59db5eaad48871d7640213179b00f46621318b6e
sergeev <you@example.com>
2024-02-14 20:56:28 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿int a = 6;
+int b = 8;
+int c = 1;
+int d = 5;
+int e = 2;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
