2012053ccb3ea51c0e78ac87681ba69ffe552141
sergeev <you@example.com>
2024-02-14 19:58:43 +0300

    push

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿int f1 = 12;
+int f2 = 24;
+int sum = f1 + f2;
+
+Console.WriteLine(sum);
+
+long Y1 = 96;
+long sum2 = Y1 + f1;
+
+Console.WriteLine(sum2);
+
+double o1 = 5.9;
+double sum3 = o1 + Y1;
+
+Console.WriteLine(sum3);
+
+int product1 = f1 * f2;
+
+Console.WriteLine(product1);
+
+long product2 = f1 * Y1;
+
+Console.WriteLine(product2);
+
+double product3 = o1 * Y1;
+
+Console.WriteLine(product3);
+
+
+
